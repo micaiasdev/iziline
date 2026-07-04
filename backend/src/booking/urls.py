0dd853f46f5fe4806608ef5692b35d47cb54b365
Edson Junior <edson.junior@ufpi.edu.br>
2026-07-04 18:41:55 -0300

    Remoção do App Booking já que a lógica foi transferida para trip

--- backend/src/booking/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from booking.apis import BookingCancelApi, BookingCreateApi, UserAgendaApi
-
-urlpatterns = [
-    path("", BookingCreateApi.as_view(), name="booking-create"),
-    path("<int:booking_id>/cancel/", BookingCancelApi.as_view(), name="booking-cancel"),
-    path("my-trips/", UserAgendaApi.as_view(), name="user-agenda"),
-]
